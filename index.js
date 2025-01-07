94d6bccf98b33290027a0e31e8248e52a60092c0
ngmanou <ngmanou3@gmail.com>
2025-01-07 11:53:42 +0100

    backend

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-res.send('Hello From Node.js');
+const morgan = require('morgan');
+
+const app =express();
+app.set('views','./view');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) =>{
+res.render('index',{ message: 'Hello From Node.js'});
 });
-app.get('/contact', (req, res) =git init> {
-res.send('The Contact Page');
+app.get('/contact', (req, res) => {
+res.render('index',{message:'The Contact Page' });
 });
 app.get('/about', (req, res) => {
-res.send('The About Page');
+res.render('index',{ message: 'The About Page'});
 });
+app.get('*',(req ,res) =>{
+});
+res.status(404).render('index',{message:'NOT Found'});
 const PORT = 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
-}); .
+}); 
